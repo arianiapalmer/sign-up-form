fa497cf2f385b448dbb344fdb1c579a5d1c0d612
Ariania Palmer <arianiapalmer@gmail.com>
2023-07-06 18:56:35 -0400

    added password logic

--- app.js
+++ app.js
@@ -1,22 +1,13 @@
 'use strict'
 let userForm = document.querySelector('form')
-
 function handleSubmit(e){
+    e.preventDefault()
     let pwd1 = e.target.pswrd.value
     let pwd2 = e.target.pswrdConf.value
     if(pwd1 === pwd2){
         return
     }else{
-        
+        alert('Passwords do not match');
     }
-    
 }
 userForm.addEventListener('submit', handleSubmit)
-
-
-
-
-// let userName = e.target.fname.value
-// let userLName = e.target.lname.value
-// let userEmail = e.target.email.value
-// let telephone = e.target.telNum.value
